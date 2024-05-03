57865fb12204d976fbdc7a6e0eb286ade30d92b2
AnasEfi <anas.efiim@gmail.com>
2024-05-03 16:13:59 +0300

    first commit

---
+++ Model_Oil/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Model_Oil
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
